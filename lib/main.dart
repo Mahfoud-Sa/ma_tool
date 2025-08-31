2cd28ea0c09c9c3291aa4fafe28f2a85654edc61
Mahfoud <binsabbah2013@gmail.com>
2025-08-31 15:21:39 +0300

    create the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:system_info2/system_info2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,97 +16,133 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  Map<String, String> systemInfo = {};
+  bool isLoading = true;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _getSystemInfo();
+  }
+
+  Future<void> _getSystemInfo() async {
+    final deviceInfoPlugin = DeviceInfoPlugin();
+    final Map<String, String> info = {};
+
+    try {
+      if (Platform.isAndroid) {
+        final androidInfo = await deviceInfoPlugin.androidInfo;
+        info['Operating System'] = 'Android ${androidInfo.version.release}';
+        info['Device'] = '${androidInfo.brand} ${androidInfo.model}';
+        info['SDK Version'] = 'API ${androidInfo.version.sdkInt}';
+      } else if (Platform.isIOS) {
+        final iosInfo = await deviceInfoPlugin.iosInfo;
+        info['Operating System'] = 'iOS ${iosInfo.systemVersion}';
+        info['Device'] = '${iosInfo.name} ${iosInfo.model}';
+      } else if (Platform.isWindows) {
+        final windowsInfo = await deviceInfoPlugin.windowsInfo;
+        info['Operating System'] = 'Windows ${windowsInfo.displayVersion}';
+        info['Device'] = windowsInfo.computerName;
+      } else if (Platform.isMacOS) {
+        final macInfo = await deviceInfoPlugin.macOsInfo;
+        info['Operating System'] = 'macOS ${macInfo.osRelease}';
+        info['Device'] = macInfo.computerName;
+      } else if (Platform.isLinux) {
+        final linuxInfo = await deviceInfoPlugin.linuxInfo;
+        info['Operating System'] = '${linuxInfo.name} ${linuxInfo.version}';
+        info['Device'] = linuxInfo.prettyName;
+      }
+
+      // Add RAM information
+      info['Total RAM'] = '${(SysInfo.getTotalPhysicalMemory() / (1024 * 1024 * 1024)).toStringAsFixed(2)} GB';
+      info['Available RAM'] = '${(SysInfo.getFreePhysicalMemory() / (1024 * 1024 * 1024)).toStringAsFixed(2)} GB';
+
+      // Add CPU information
+      info['CPU Cores'] = SysInfo.cores.length.toString();
+      info['CPU Architecture'] = Platform.operatingSystem;
+
+    } catch (e) {
+      info['Error'] = 'Unable to get system information: $e';
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      systemInfo = info;
+      isLoading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("System Information"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView(
+                children: [
+                  const Text(
+                    'System Details',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  ...systemInfo.entries.map((entry) => _buildInfoCard(entry.key, entry.value)),
+                ],
+              ),
+            ),
+    );
+  }
+
+  Widget _buildInfoCard(String title, String value) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Text(
+                '$title:',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Text(
+                value,
+                style: const TextStyle(fontSize: 16),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
